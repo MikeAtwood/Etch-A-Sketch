9791d02efd07f55a113effd49913f4a3bd4a6a06
Deadpool1 <atwood169@yahoo.com>
2023-03-08 13:57:01 -0600

    added clear functionality

--- index.js
+++ index.js
@@ -4,21 +4,36 @@ const rainbowBtn = document.querySelector('.rainbow')
 const eraserBtn = document.querySelector('.eraser')
 const clearBtn = document.querySelector('.clear')
 
+let color = 'black';
+
+
+clearBtn.addEventListener('click', clearBoard)
+
+// This creates our grid tables for users to draw
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
-        cell.textContent = ('');
+        cell.addEventListener("mouseover", squareColor);
+        cell.addEventListener("mousedown", squareColor);
+        cell.style.backgroundColor = "white";
         container.appendChild(cell).className = "grid-item";
     };
 };
 makeRows(16, 16)
 
 
+// Defines the default color
+function squareColor() {
+    this.style.backgroundColor = color;
+} 
 
-colorBtn.addEventListener('click', function(){
-    console.log('Clicked')
+
+
+
+colorBtn.addEventListener('click', function(e){
+    cell.style.backgroundColor = 'red'
 })
 
 rainbowBtn.addEventListener('click', function(){
@@ -26,9 +41,16 @@ rainbowBtn.addEventListener('click', function(){
 })
 
 eraserBtn.addEventListener('click', function(){
-    console.log('Destroy this div')
+    let cells = document.querySelector('div')
+    cells.style.backgroundColor = 'white'
 })
 
-clearBtn.addEventListener('click', function(){
-    console.log('Muah-hahahaha')
-})
+
+
+
+// function the clearBtn calls to clear the board
+function clearBoard() {
+    let container = document.querySelector('#container')
+    let cells = container.querySelectorAll('div')
+    cells.forEach((div) => div.style.backgroundColor = 'white')
+}
